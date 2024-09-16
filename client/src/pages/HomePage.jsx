47349c002589984e828b41837fbf74462a482e35
mitul88 <shahed.rahman88@gmail.com>
2024-09-16 18:03:46 +0600

    home page layout done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <div className="container-fluid text-center p-5">
+        <h2>Welcome to Whiteboard</h2>
+        <h5>Draw shapes and lines</h5>
+        <a href="/whiteboard" className="btn btn-primary ">
+          Draw
+        </a>
+      </div>
+      <div className="container">
+        <div className="row"></div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
