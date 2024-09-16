fd5ea1900f59ddc6fa7887742720567a9a0b5560
mitul88 <shahed.rahman88@gmail.com>
2024-09-16 18:17:07 +0600

    home page done

--- client/src/components/DrawingList.jsx
+++ client/src/components/DrawingList.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import DrawingItem from "./DrawingItem";
 
 const DrawingList = () => {
-  return <div>DrawingList</div>;
+  return (
+    <div className="row text-center ">
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+        <DrawingItem />
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+        <DrawingItem />
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+        <DrawingItem />
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+        <DrawingItem />
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+        <DrawingItem />
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+        <DrawingItem />
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5">
+        <DrawingItem />
+      </div>
+    </div>
+  );
 };
 
 export default DrawingList;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import DrawingList from "../components/DrawingList";
 
 const HomePage = () => {
   return (
-    <div>
-      <div className="container-fluid text-center p-5">
+    <div className="bg-light">
+      <div className="container-fluid bg-dark text-white text-center p-5">
         <h2>Welcome to Whiteboard</h2>
         <h5>Draw shapes and lines</h5>
         <a href="/whiteboard" className="btn btn-primary ">
           Draw
         </a>
       </div>
-      <div className="container">
-        <div className="row"></div>
+      <div className="container text-center">
+        <h3 className="my-5">View All drawings</h3>
+        <DrawingList />
       </div>
     </div>
   );
